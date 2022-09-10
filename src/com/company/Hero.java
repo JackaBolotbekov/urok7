d86aa41e9e235d27a3ff1d55269f6ceeb145e98f
JackaBolotbekov <jackabolotbekow@gmail.com>
2022-09-10 20:18:32 +0300

    urok7

---
+++ src/com/company/Hero.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+
+
+
+public abstract class Hero implements HavingSuperAbility {
+    private int HM;
+    private int DM;
+    private   String power;
+
+
+
+
+
+}
---
+++ src/com/company/Warrior.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+public class Warrior extends Hero {
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Warrior applied super ability Enargy Attak");
+
+
+    }
+}
